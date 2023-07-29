8a9ac8fcf92975f4d0b51d5e7e1e5af24ece0776
elasheras <emperorbfd@gmail.com>
2023-07-29 20:41:34 -0300

    MINOR. FEATURE. Added animations for the fade in text. This was done over all the week in between the online web-dev course im taking. Felt bad not to add it to the actual project even though we still do not see JS.

---
+++ js/index.js
@@ -0,0 +1,74 @@
+function init() {
+    console.log('initialized js');
+    $("p").each(function (index) {
+        fadeInTextInElement(index, $(this));
+    });
+
+}
+
+function fadeInTextInElement(index, paragraph) {
+    // const paragraph = $(this);
+    const textContent = paragraph.text();
+
+    // Clear the original content of the paragraph
+    paragraph.text('');
+
+    // const localDelay = getRandomValue(15, 30);
+    const localDelay = getRandomValue(1, 8);
+    // Iterate through each character in the text content
+    for (let i = 0; i < textContent.length; i++) {
+        let char = textContent.charAt(i);
+        // Wrap each letter in a <span> element
+        let span = $("<span>")
+            .text(char)
+            .css("display", "inline")
+            // .css("opacity", 1)
+            .addClass('s6-animate-accent-to-primary')
+            .appendTo(paragraph)
+            .hide()
+            .delay(index * 100 + (i * localDelay))
+            .fadeIn(100);
+
+        if (brandMatch(char) && runProbability(10)) span.css("filter", "hue-rotate(5deg)"); // <-- just an easteregg
+
+    }
+}
+
+function brandMatch(char) {
+    const regex = /[syx6]/i;
+    return regex.test(char)
+}
+
+
+function runProbability(pct) {
+    // Generate a random number between 0 and 100 (inclusive of 0, exclusive of 100)
+    const randomNum = Math.random() * 100;
+
+    // Check if the random number is less than 2 (2 percent chance)
+    if (randomNum < pct) {
+        // Do something here (the action with 2 percent probability)
+        console.log("Action executed with 2 percent chance.");
+        return true;
+    } else {
+        // console.log("Action not executed.");
+        return false;
+    }
+}
+
+function getRandomValue(min, max) {
+    // Calculate the range of values (inclusive of min and max)
+    const range = max - min + 1;
+
+    // Generate a random number between 0 and range - 1
+    const randomOffset = Math.floor(Math.random() * range);
+
+    // Add the random offset to the min value to get the final random value
+    const randomValue = min + randomOffset;
+
+    return randomValue;
+}
+
+
+
+// run init function
+init();
